3370a42a28b20d19ae1123494c85690e9d1d1fc8
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-12 22:17:39 +0100

    creating typography for project

---
+++ src/utils/typography.js
@@ -0,0 +1,8 @@
+import Typography from "typography"
+const typography = new Typography({
+    baseFontSize: "16px",
+    bodyFontFamily: ["Montserrat"],
+    headerFontFamily: ["Montserrat"],
+})
+
+export default typography
