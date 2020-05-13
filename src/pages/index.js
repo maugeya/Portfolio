7c9b5420061f266883e30ff8fad65bcadc7a9bcd
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-13 22:19:35 +0100

    Adding navbar component and updating imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,26 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../components/layout/layout"
+import Navbar from "../components/navbar/navbar"
 import SEO from "../components/seo"
-import Jumbo from "../components/jumbo"
-import AboutMe from "../components/aboutMe"
-import Contact from "../components/contact"
-import Projects from "../components/projects"
+import Jumbo from "../components/jumbo/jumbo"
+import AboutMe from "../components/aboutMe/aboutMe"
+import Contact from "../components/contact/contact"
+import Projects from "../components/projects/projects"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Jumbo />
-    <AboutMe />
-    <Contact />
-    <Projects />
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
+  <>
+    <Navbar />
+    <Layout>
+      <SEO title="Home" />
+      <Jumbo />
+      <AboutMe />
+      <Contact />
+      <Projects />
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
+    </Layout>
+  </>
 )
 
 export default IndexPage
