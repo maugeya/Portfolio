726bca05fc219dd886cdfb70c175e070f1ca48af
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-13 22:19:49 +0100

    Creating Navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+import { AnchorLink } from "gatsby-plugin-anchor-links";
+import navStyles from "./navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div className={navStyles.navbar}>
+            <div>LOGO</div>
+            <ul className={navStyles.navLinkList}>
+                <li className={navStyles.navLinkListItem}>
+                    <AnchorLink className={navStyles.navLink} to="/#aboutMe">ABOUT</AnchorLink>
+                </li>
+                <li className={navStyles.navLinkListItem}>
+                    <AnchorLink className={navStyles.navLink} to="/#projects">PROJECTS</AnchorLink>
+                    </li>
+                <li className={navStyles.navLinkListItem}>
+                    <AnchorLink className={navStyles.navLink} to="/#contact">CONTACT</AnchorLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
