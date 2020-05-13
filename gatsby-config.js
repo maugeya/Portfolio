cb9b3505dae9d3a4f6d719049b28ad5c964d3665
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-13 22:17:47 +0100

    adding scroll plugin for nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
@@ -42,6 +42,9 @@ module.exports = {
         ],
         display: 'swap'
       }
+    },
+    {
+      resolve: "gatsby-plugin-anchor-links",
     }
   ],
 }
