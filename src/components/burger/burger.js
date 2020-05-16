6ceeb072dea41c6a578f571cf8a68345a58c4d99
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-16 15:10:51 +0100

    Using link for links to sections on same page

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { Link } from "gatsby"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 import burgerStyles from "./burger.module.css"
 
@@ -27,25 +28,31 @@ const HamburgerMenu = () => {
             </label>
             <nav>
                 <ul className={burgerStyles.burgerList}>
+                    <li className={burgerStyles.burgerListItem}>
+                        <Link to="#about" onClick={handleOpenMenu} className={burgerStyles.burgerListLink}>ABOUT</Link>
+                    </li>
                     <li className={burgerStyles.burgerListItemWithSubMenu}>
-                        <a  className={burgerStyles.burgerListLink} href="#">Projects</a>
+                        <Link className={burgerStyles.burgerListLink} to="#projects"  onClick={handleOpenMenu}>PROJECTS</Link>
                         <ul className={burgerStyles.burgerListSubMenu}>
                             <li className={burgerStyles.burgerListSubMenuItem}>
-                                <AnchorLink to="/project-1" onClick={handleOpenMenu}>PROJECT 1</AnchorLink>
+                                <AnchorLink to="/project-1" onClick={handleOpenMenu} className={burgerStyles.burgerSubMenuLink}>PROJECT 1</AnchorLink>
                             </li>
                             <li className={burgerStyles.burgerListSubMenuItem}>
-                                <AnchorLink to="/project-2" onClick={handleOpenMenu}>PROJECT 2</AnchorLink>
+                                <AnchorLink to="/project-2" onClick={handleOpenMenu} className={burgerStyles.burgerSubMenuLink}>PROJECT 2</AnchorLink>
                             </li>
                             <li className={burgerStyles.burgerListSubMenuItem}>
-                                <AnchorLink to="/project-3" onClick={handleOpenMenu}>PROJECT 3</AnchorLink>
+                                <AnchorLink to="/project-3" onClick={handleOpenMenu} className={burgerStyles.burgerSubMenuLink}>PROJECT 3</AnchorLink>
                             </li>
                             <li className={burgerStyles.burgerListSubMenuItem}>
-                                <AnchorLink to="/project-4" onClick={handleOpenMenu}>PROJECT 4</AnchorLink>
+                                <AnchorLink to="/project-4" onClick={handleOpenMenu} className={burgerStyles.burgerSubMenuLink}>PROJECT 4</AnchorLink>
                             </li>
                             <li className={burgerStyles.burgerListSubMenuItem}>
-                                <AnchorLink to="/project-5" onClick={handleOpenMenu}>PROJECT 5</AnchorLink>
+                                <AnchorLink to="/project-5" onClick={handleOpenMenu} className={burgerStyles.burgerSubMenuLink}>PROJECT 5</AnchorLink>
                             </li>
                         </ul>
+                    </li>
+                    <li className={burgerStyles.burgerListItem}>
+                        <Link to="#contact" onClick={handleOpenMenu} className={burgerStyles.burgerListLink}>CONTACT</Link>
                     </li>
                 </ul>
             </nav>
