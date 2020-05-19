782192573bed4d79dcb8d3ac2deb435d29cc3f47
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-19 21:25:20 +0100

    Removing unrequired link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout/layout"
 import Navbar from "../components/navbar/navbar"
@@ -18,7 +17,6 @@ const IndexPage = () => (
       <AboutMe />
       <Contact />
       <Projects />
-      {/* <Link to="/page-2/">Go to page 2</Link> */}
     </Layout>
   </>
 )
