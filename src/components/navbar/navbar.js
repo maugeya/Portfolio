dff9e56478611731209d1d63bc71f65672269f2c
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-19 21:24:37 +0100

    Updating logic for showing submenu to include state to have easier changes to submenu showing and triangle icon movement

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 import navStyles from "./navbar.module.css"
 import HamburgerMenu from "../burger/burger"
 
 const Navbar = () => {
+    const [subMenuOpen, setSubMenuOpen] = useState(false)
+    console.log(subMenuOpen)
+
+    const handleSubMenuOpen = () => {
+        setSubMenuOpen(true)
+    }
+
+    const handleSubMenuClose = () => {
+        setSubMenuOpen(false)
+    }
+
+    const handleSubMenuClassName = (navStyles) => {
+        if (!subMenuOpen) {
+            return navStyles.subMenuHidden
+        }
+        return navStyles.subMenu
+    }
+
+    const handleTriangleIconPosition = (navStyles) => {
+        if (!subMenuOpen) {
+            return navStyles.triangleIcon
+        }
+        return navStyles.activeTriangleIcon
+    }
+
     return (
         <div className={navStyles.navbar}>
             <AnchorLink to="/#jumbo">
@@ -14,17 +39,21 @@ const Navbar = () => {
                 <li className={navStyles.navLinkListItem}>
                     <AnchorLink className={navStyles.navLink} to="/#about">ABOUT</AnchorLink>
                 </li>
-                <li className={navStyles.navLinkListItem}>
-                    <AnchorLink className={navStyles.navLink} to="/#projects">
-                        PROJECTS
-                    </AnchorLink>
-                    <ul className={navStyles.subMenu}>
+                <li className={navStyles.navLinkListItem}  onMouseLeave={handleSubMenuClose} onMouseEnter={handleSubMenuOpen} >
+                    <div className={navStyles.projectLinkContainer}>
+                        <AnchorLink className={navStyles.navLink} to="/#projects">
+                            PROJECTS
+                        </AnchorLink>
+                        <div className={navStyles.triangleIconContainer}>
+                            <div className={handleTriangleIconPosition(navStyles)} />
+                        </div>
+                    </div>
+                    <ul className={handleSubMenuClassName(navStyles)}>
                         <li className={navStyles.subMenuListItems}>
                             <AnchorLink className={navStyles.navSubMenu} to="/project-1">
                                 LGBT Clinic Search
                             </AnchorLink>
                         </li>
-
                         <li className={navStyles.subMenuListItems}>
                             <AnchorLink className={navStyles.navSubMenu} to="/project-2">
                                 Project 2
