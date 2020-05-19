61e83dc4f1ac3a1883df07966733fdd3a3195ee1
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-19 21:25:10 +0100

    Beginnings of jumbo development

--- src/components/jumbo/jumbo.js
+++ src/components/jumbo/jumbo.js
@@ -1,8 +1,14 @@
 import React from "react"
+import jumboStyles from "./jumbo.module.css"
 
 const Jumbo = () => (
     <div id="jumbo" className="section">
-        <h2>JUMBO HERE</h2>
+        <div className={jumboStyles.jumboContainer}>
+          <div className={jumboStyles.jumboText}>
+            <h1>Hello,<br /> I'm <span>Angela</span>, <br />a Software Engineer.</h1>
+          </div>
+          <div className={jumboStyles.angeBackgroundImg} />
+        </div>
     </div>
 )
 
