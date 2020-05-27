b5fab0e022a99868ce5f320c0ee5b190e1196b9e
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-27 14:28:30 +0100

    Adding some basics into about me. TODO: need to complete this

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -2,8 +2,20 @@ import React from "react"
 import aboutMeStyles from "./aboutMe.module.css"
 
 const AboutMe = () => (
-    <div id="about" className="section">
+    <div id="about" className={`section ${aboutMeStyles.aboutMeContainer}`}>
         <h2 className="sectionHeader">About me</h2>
+        <div className={aboutMeStyles.content}>
+        <p>
+            I've always been interested in computers but was a chef for 8 years before re-skilling to become a software engineer.
+            My interest started as a hobby but I now work as a fullstack engineer after seeing others code within my household.
+        </p>
+        <p>
+            I love the problem solving element to my role and the chance to be continually learning new skills and putting them into practise.
+        </p>
+        </div>
+        
+            
+        
     </div>
 )
 
