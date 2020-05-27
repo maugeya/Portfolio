8ea1d9703a5b209ce9b8821d8126d204b7951f9b
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-27 14:28:04 +0100

    Updating title and logo info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Angela Rice`,
+    description: `Angela Rice Software Engineer Portfolio`,
+    author: `Angela Rice`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/ange_logo.png`,
       },
     },
     {
