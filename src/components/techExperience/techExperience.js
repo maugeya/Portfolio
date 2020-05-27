76ab1daa8993fcfa3ea037ea0b42f53604c31105
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-05-27 14:29:56 +0100

    Creating tech experience component

---
+++ src/components/techExperience/techExperience.js
@@ -0,0 +1,44 @@
+import React from "react"
+import techExpStyles from "./techExperience.module.css"
+import PythonIcon from "../svgIcons/pythonIcon"
+import HtmlIcon from "../svgIcons/htmlIcon"
+import CssIcon from "../svgIcons/cssIcon"
+import SassIcon from "../svgIcons/sassIcon"
+import GitIcon from "../svgIcons/gitIcon"
+import DockerIcon from "../svgIcons/dockerIcon"
+import JavaScriptIcon from "../svgIcons/javascriptIcon"
+import ReactIcon from "../svgIcons/reactIcon"
+import ReduxIcon from "../svgIcons/reduxIcon"
+import DjangoIcon from "../svgIcons/djangoIcon"
+import GraphQLIcon from "../svgIcons/graphQLIcon"
+import JestIcon from "../svgIcons/jestIcon"
+import MySQLIcon from "../svgIcons/mySqlIcon"
+import JqueryIcon from "../svgIcons/jqueryIcon"
+
+const TechExperience = () => {
+    return (
+        <div id="about" className={`section ${techExpStyles.techContainer}`}>
+        <h2 className="sectionHeader">Tech experience</h2>
+        <div className={techExpStyles.techIconContainer}>
+            <HtmlIcon />
+            <CssIcon />
+            <SassIcon />
+            <JavaScriptIcon />
+            <JqueryIcon />
+            <ReactIcon />
+            <ReduxIcon />
+            <PythonIcon />
+            <DjangoIcon />
+            <GraphQLIcon />
+            <JestIcon />
+            <MySQLIcon />
+            <GitIcon />
+            <DockerIcon />
+        </div>
+        
+
+    </div>
+    )
+}
+
+export default TechExperience
