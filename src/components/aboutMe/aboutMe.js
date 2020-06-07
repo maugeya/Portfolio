144a5d0e7e43d7d4049b5051ff0b5c7d32aaae64
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-07 14:02:05 +0100

    Updates to about me text

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -6,11 +6,18 @@ const AboutMe = () => (
         <h2 className="sectionHeader">About me</h2>
         <div className={aboutMeStyles.content}>
         <p>
-            I've always been interested in computers but was a chef for 8 years before re-skilling to become a software engineer.
-            My interest started as a hobby but I now work as a fullstack engineer after seeing others code within my household.
+            A little bit about me...I've always been interested in computers but I was a chef for 8 years before re-skilling to become a software engineer.
+            My interest started as a hobby but I now work as a fullstack engineer, with a little bit of mobile development thrown in as well.
         </p>
         <p>
             I love the problem solving element to my role and the chance to be continually learning new skills and putting them into practise.
+            I keep the end user in mind when working and add meaning input as to what I feel could help the product across teams.
+        </p>
+        <p>
+            I believe in intuitive websites, making it easy for the end user to get the most out of a site and have a passion for building simple yet stylish products.
+        </p>
+        <p>
+            Check out my projects and contact me if you like what you see!
         </p>
         </div>
         
