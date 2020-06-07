92530915a3d41c4d0847be6c3c51a6a94f9b36f3
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-07 14:02:43 +0100

    Adding images with styles

--- src/components/jumbo/jumbo.js
+++ src/components/jumbo/jumbo.js
@@ -1,5 +1,10 @@
 import React from "react"
 import jumboStyles from "./jumbo.module.css"
+import ange_1 from "../../images/heroImg/ange_1.png"
+import ange_2 from "../../images/heroImg/ange_2.png"
+import ange_3 from "../../images/heroImg/ange_3.png"
+import ange_4 from "../../images/heroImg/ange_4.png"
+import ange_5 from "../../images/heroImg/ange_5.png"
 
 const Jumbo = () => (
     <div id="jumbo" className="section">
@@ -7,7 +12,13 @@ const Jumbo = () => (
           <div className={jumboStyles.jumboText}>
             <h1>Hello,<br /> I'm <span className={jumboStyles.nameSpan}>Angela</span>, <br />a Software Engineer.</h1>
           </div>
-          <div className={jumboStyles.angeBackgroundImg} />
+          <div className={jumboStyles.angeImageContainer}>
+            <img className={jumboStyles.angeImage} src={ange_1}/>
+            <img className={jumboStyles.angeImage}  src={ange_2}/>
+            <img className={jumboStyles.angeImage}  src={ange_3}/>
+            <img className={jumboStyles.angeImage}  src={ange_4}/>
+            <img className={jumboStyles.angeImage}  src={ange_5}/>
+          </div>
         </div>
     </div>
 )
