48e6f84b8f645cd325a7008047ea00e57b052087
Angela Maugey <angelamaugey@Angelas-MacBook-Pro.local>
2020-06-14 13:25:53 +0100

    Remove and delete unrequired component

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "../header/header"
 import Footer from "../footer/footer"
 import layoutStyles from "./layout.module.css"
 
